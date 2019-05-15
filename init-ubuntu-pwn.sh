246ecc4578a142362c1d45765ff56be6a01c5826
Guoker3 <47735844+Guoker3@users.noreply.github.com>
2019-05-16 00:46:19 +0800

    including: source update upgrade vim 32bit pip git
    including: source update upgrade vim 32bit pip git

---
+++ init-ubuntu-pwn.sh
@@ -0,0 +1,40 @@
+sudo mv /etc/apt/sources.list /etc/apt/sources.list.bak
+echo 'deb http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse' > /etc/apt/sources.list
+echo 'deb-src http://mirrors.aliyun.com/ubuntu/ bionic main restricted universe multiverse' >> /etc/apt/sources.list
+echo 'deb http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse' >> /etc/apt/sources.list
+echo 'deb-src http://mirrors.aliyun.com/ubuntu/ bionic-security main restricted universe multiverse' >> /etc/apt/sources.list
+echo 'deb http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse' >> /etc/apt/sources.list
+echo 'deb-src http://mirrors.aliyun.com/ubuntu/ bionic-updates main restricted universe multiverse' >> /etc/apt/sources.list
+echo 'deb http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse' >> /etc/apt/sources.list
+echo 'deb-src http://mirrors.aliyun.com/ubuntu/ bionic-backports main restricted universe multiverse' >> /etc/apt/sources.list
+echo 'deb http://mirrors.aliyun.com/ubuntu/ bionic-proposed main restricted universe multiverse' >> /etc/apt/sources.list
+echo 'deb-src http://mirrors.aliyun.com/ubuntu/ bvionic-proposed main restricted universe multiverse' >> /etc/apt/sources.list
+
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get install vim
+
+sudo apt-get install  lib32ncurses5 lib32z1
+sudo apt install python-pip
+
+sudo apt install git
+
+sudo git clone https://github.com/longld/peda.git ~/peda
+sudo echo "source ~/peda/peda.py" >> ~/.gdbinit
+
+cd /
+sudo mkdir git-misc-re
+
+cd /git-misc-re
+sudo git clone https://github.com/aquynh/capstone
+cd /git-miscre/capstone
+sudo make
+sudo make install
+pip install setuptools
+
+cd /git-misc-re
+sudo git clone https://github.com/Gallopsled/pwntools
+cd /git-misc-re/pwntools
+sudo python setup.py install
+
